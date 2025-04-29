ecbbce814c2d72d5aa603c2eeef3de1c166f24ec
MeherabHossain007 <meherab788@gmail.com>
2025-04-29 23:20:17 +0600

    Refactor import paths for Navbar and Footer components in layout.tsx

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,17 @@
 // next.config.ts
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [new URL('http://localhost:1337/**')],
+    remotePatterns: [new URL(`${process.env.NEXT_PUBLIC_STRAPI_API_URL}/**`)],
+  },
+  webpack(config) {
+    config.resolve.alias = {
+      ...(config.resolve.alias ?? {}),
+      "@": path.resolve(__dirname, "."),
+    };
+    return config;
   },
 };
 
